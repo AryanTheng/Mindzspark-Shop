3e97c4d60e8da958d17d57912d5cbf66e010ada7
AryanTheng <162592894+AryanTheng@users.noreply.github.com>
2025-06-22 19:43:02 +0530

    22-06-2025(Sunday): Implemented Razorpay on CheckoutPage.

--- server/route/order.route.js
+++ server/route/order.route.js
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import auth from '../middleware/auth.js'
-import { CashOnDeliveryOrderController, getOrderDetailsController, paymentController } from '../controllers/order.controller.js'
+import { CashOnDeliveryOrderController, createRazorpayOrder, getOrderDetailsController, handleWebhook, paymentController, verifyRazorpayPayment } from '../controllers/order.controller.js'
 import razorpay from '../config/razorpay.js'
 
 const orderRouter = Router()
 
 orderRouter.post("/cash-on-delivery",auth,CashOnDeliveryOrderController)
-orderRouter.post('/checkout',auth,paymentController)
+orderRouter.post('/create-razorpay-order',auth,createRazorpayOrder)
+orderRouter.post('/verify-razorpay-payment',auth,verifyRazorpayPayment)
+orderRouter.post('/web-hook',auth,handleWebhook)
 orderRouter.get("/order-list",auth,getOrderDetailsController)
 
 // Add Razorpay order creation endpoint stub
